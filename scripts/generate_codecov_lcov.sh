2fa237978527d5c3c29e67c65cc17c78139be5eb
bymoye <s3moye@gmail.com>
2026-09-12 17:47:51 +0800

    ci: pull pinned MinIO image from accessible Quay registry

--- scripts/generate_codecov_lcov.sh
+++ scripts/generate_codecov_lcov.sh
@@ -204,7 +204,7 @@ docker run -d --name "$MINIO_CONTAINER" \
   --publish 127.0.0.1:9000:9000 \
   --env MINIO_ROOT_USER=sharedstate-test \
   --env MINIO_ROOT_PASSWORD=sharedstate-test-only \
-  docker.io/minio/minio@sha256:14cea493d9a34af32f524e538b8346cf79f3321eff8e708c1e2960462bd8936e \
+  quay.io/minio/minio@sha256:14cea493d9a34af32f524e538b8346cf79f3321eff8e708c1e2960462bd8936e \
   server /data
 
 services_ready=false
